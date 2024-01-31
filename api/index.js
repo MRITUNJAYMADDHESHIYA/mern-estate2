638d8eaf971406481f316b16926a0e75147c52a6
Mritunjay Maddhesiya <mritunjay112002@gmail.com>
2024-01-31 21:43:08 +0530

    create a test api route

--- api/index.js
+++ api/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import userRouter from './routes/user.route.js';
 import dotenv from 'dotenv';  //this packege use for hide the database link in env
 dotenv.config();
 
@@ -15,3 +16,5 @@ const app = express();
 app.listen(3000, ()=>{
     console.log('Server is running on port 3000!!!')
 })
+
+app.use("/api/user", userRouter)
